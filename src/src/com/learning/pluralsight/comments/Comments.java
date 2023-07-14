1355362a7871e2cec7f76917f20d82a4dec6e561
sakethkamatham <sakethkamatham@gmail.com>
2023-07-14 13:55:25 -0400

    Commit 2 : Added Details about different comments

---
+++ src/src/com/learning/pluralsight/comments/Comments.java
@@ -0,0 +1,29 @@
+package com.learning.pluralsight.comments;
+
+/**
+ * This class illustrates different types of comments in Java
+ *
+ * Run the main class to see the print Statements
+ *
+ * @Author : Saketh Kamatham
+ */
+public class Comments {
+
+    public void LineComment(){
+        System.out.println("In the comments Class for Line Comment"); //This is for the line comment
+    }
+
+    public void BlockComment(){
+        System.out.println("In the comments Class for Block Comment");
+        /*
+        This is for the block comment
+         */
+    }
+
+    public void JavaDocComment(){
+        System.out.println("In the comments Class for Javadoc Comment");
+        /**
+         * This is for documentation
+         */
+    }
+}
--- src/src/com/learning/pluralsight/myfirstprogram/HelloWorld.java
+++ src/src/com/learning/pluralsight/myfirstprogram/HelloWorld.java
@@ -1,12 +1,27 @@
 package com.learning.pluralsight.myfirstprogram;
 
+import com.learning.pluralsight.comments.Comments;
+
 /**
  * Basic Hello World Program
  *
  * @Author : Saketh Kamatham
  */
 public class HelloWorld {
+
+     static Comments comments = new Comments();
     public static void main(String[] args) {
+        helloWorld();
+        commentDemo();
+    }
+
+    public static void helloWorld(){
         System.out.println("Hello world!");
+    }
+
+    public static void commentDemo(){
+        comments.LineComment();
+        comments.BlockComment();
+        comments.JavaDocComment();
     }
 }
