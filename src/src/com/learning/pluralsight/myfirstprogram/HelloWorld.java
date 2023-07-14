70d99156789f1f5110a15f4ba60bd96ee7aeecca
sakethkamatham <sakethkamatham@gmail.com>
2023-07-14 13:33:58 -0400

    Commit 1: Basic Project Setup

---
+++ src/src/com/learning/pluralsight/myfirstprogram/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.learning.pluralsight.myfirstprogram;
+
+/**
+ * Basic Hello World Program
+ *
+ * @Author : Saketh Kamatham
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
